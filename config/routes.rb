675446e18ca7a459d164f19221c3f5ab983715e1
coalva94 <coalva.94@gmail.com>
2022-02-01 16:05:36 -0500

    Routes generados

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,26 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tours do
+    resources :bookings, only: %i[create new]
+  end
+  resources :bookings, only: %i[index show edit destroy update]
 end
+
+
+# /	GET	pages	home
+# /tours	GET	offers	index
+# /tours/:id	GET	tours	show
+# /tours/:id/questions/:id	POST 	questions	create
+# /tours/:id/bookings/:id	POST 	bookings	create
+# users/id/bookings	GET	users	index
+# /tours/:id/bookings/:id	POST 	booking	create
+# tours/:id/bookings/:id/edit	PATCH	booking	update
+# tours/:id/bookings/:id/	DELETE	booking	desttoy
+# tours/new	GET	tour	new
+# tours	POST 	tours	create
+# tours/:id/edit	GET	tours	edit
+# tours/:id	PATCH	tour	update
+# tours/:id	DELETE	tour 	destroy
+# tour/id/question	POST 	question	create
+# bookings	GET	bookings	index
