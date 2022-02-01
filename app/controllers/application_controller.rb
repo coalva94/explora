1b979204a2f299f621d0b37bbd2d444880a5d6b3
coalva94 <coalva.94@gmail.com>
2022-02-01 12:26:32 -0500

    Instalacion devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name phone_number document_number user_type])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone_number document_number user_type])
+  end
+
 end
