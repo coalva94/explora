8a731d8860995e27921763a8f232f605e438d2c1
Cristopher <cristopheer96@gmail.com>
2022-02-03 01:58:37 -0500

    Accabando Home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @tours_near = Tour.all + Tour.all + Tour.all # debemos filtrar los Tours que esten cerca del lugar donde el usuario abre nuestra paginas
   end
 end
