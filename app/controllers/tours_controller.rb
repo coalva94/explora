4e9cfbfb5e6b1903d4aa39028032727e858fb29e
Horlando Espinoza <orland20161@gmail.com>
2022-02-07 18:18:57 -0500

    se agrego el search

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,7 +1,17 @@
 class ToursController < ApplicationController
 
   def index
-    @tours = Tour.all
+    if params[:query].present?
+      sql_query = "\
+      tours.title_tour ILIKE :query\
+      OR tours.description ILIKE :query\
+      OR tours.duration ILIKE :query\
+      OR agencies.name ILIKE :query\
+      "
+      @tours = Tour.joins(:agency).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @tours = Tour.all
+    end
   end
 
   def show
