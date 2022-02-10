d8ca30dfc578792766e9af533c2c551ee5c2936f
Andres Munoz <andremude@hotmail.com>
2022-02-10 16:29:10 -0500

    error solucionado

--- app/models/companion.rb
+++ app/models/companion.rb
@@ -3,5 +3,5 @@ class Companion < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :age, presence: true
-  validates :document_number, length: { is: 8 },
+  validates :document_number, length: { is: 8 }
 end
