c0dac181939922764772cc1609f6b143f8d948aa
coalva94 <coalva.94@gmail.com>
2022-02-10 16:06:27 -0500

    dni

--- app/models/companion.rb
+++ app/models/companion.rb
@@ -1,9 +1,7 @@
 class Companion < ApplicationRecord
   belongs_to :booking
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :age, presence: true
-  validates :document_number, presence: true
-
+  validates :document_number, length: { is: 8 },
 end
